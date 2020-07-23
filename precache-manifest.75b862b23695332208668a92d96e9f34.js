8211e8780d4e54ea70221536b03997b26eb3b226
EC2 Default User <ec2-user@ip-172-31-91-132.ec2.internal>
2020-07-23 07:05:06 +0000

    Updates

---
+++ precache-manifest.75b862b23695332208668a92d96e9f34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65846facd7155e38c0a0c946f6740f3e",
+    "url": "/myfirstapp/index.html"
+  },
+  {
+    "revision": "26bb80bc7a106a360f5f",
+    "url": "/myfirstapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d17bfd69ac6143c53986",
+    "url": "/myfirstapp/static/js/2.f7c890b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myfirstapp/static/js/2.f7c890b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26bb80bc7a106a360f5f",
+    "url": "/myfirstapp/static/js/main.5064f9f6.chunk.js"
+  },
+  {
+    "revision": "70923609ecd1612396c9",
+    "url": "/myfirstapp/static/js/runtime-main.332fd6c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myfirstapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
